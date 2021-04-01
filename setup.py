57c8343543e45d86cb653f520db094e63ec952ab
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2021-04-01 17:47:38 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'dash_simply',
+    packages = ['dash_simply'],
+    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Create dash view objects easily with reusable methods.',
+    author = 'Enun Bassey Enun',
+    author_email = 'enunenun21@gmail.com',
+    url = 'https://github.com/EJ-enun/dash_simply/',
+    download_url = 'https://github.com/EJ-enun/dash_simply/archive/refs/tags/1.0.tar.gz',
+    keywords = ['Dash', 'Python'],
+    classifiers = [],
+)
